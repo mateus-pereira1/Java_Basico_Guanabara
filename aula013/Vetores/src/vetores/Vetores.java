886141ab670b902dfef1ae6c9257dc37db386429
Mateus Pereira <mateuspereira@email.com>
2025-08-16 15:06:48 -0300

    Aula 14
    Adicionado os exercícios da aula 14, referente a vetores.

---
+++ aula013/Vetores/src/vetores/Vetores.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vetores;
+
+/**
+ *
+ * @author Mateus
+ */
+public class Vetores {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int v[] = {5,7,8,9,6,2};
+        System.out.println(v.length);
+        for(int c = 0; c < v.length; c++){
+            System.out.print(v[c]);
+        }
+    }
+    
+}
