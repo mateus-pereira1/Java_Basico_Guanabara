eb9cd0c64b6c6392997e18efa03480a43b9d9266
Mateus Pereira <mateuspereira@email.com>
2025-08-17 12:18:22 -0300

    Adicionado
    Adicionado as últimas aulas do Java básico do curso em vídeo.

--- aula014/Vetor02/src/vetor02/Vetor02.java
+++ aula014/Vetor02/src/vetor02/Vetor02.java
@@ -15,12 +15,20 @@ public class Vetor02 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        String mes[] = {"Jan","Fev","Mar","Abr","Mai","Jun","Jul","Ago","Set","Out","Nov","Dez"};
+        /*String mes[] = {"Jan","Fev","Mar","Abr","Mai","Jun","Jul","Ago","Set","Out","Nov","Dez"};
         int tot[] = {31,28,31,30,31,30,31,31,30,31,30,31};
         
         for(int c =0; c < mes.length; c++){
             System.out.println("O mês de "+mes[c]+" tem "+tot[c]+" Dias ao Todo");
-        }
+        }*/
+       int x = 5;
+       int y = 2;
+       double z;
+       
+       z = x/y;
+       
+       System.out.print(z);
+        
     }
     
 }
